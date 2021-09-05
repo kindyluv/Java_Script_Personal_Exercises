51020f53d212ab606531e1afc88d138b14c1a731
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-05 18:11:23 +0100

    Add files via upload

---
+++ exercise.js
@@ -0,0 +1,10 @@
+let myArray = [["precious", 23, "short",], ["isreal", 27, "tall",], ["olatoye", 27, "very short",]];
+console.log(myArray);
+
+let yourArray = [["Adesuwa", 30, "tall",], ["kimberly", 25, "tall",]] + myArray;
+console.log(yourArray);
+
+myArray.push(["Goodnews", 28, "tall",]);
+console.log(myArray);
+
+
